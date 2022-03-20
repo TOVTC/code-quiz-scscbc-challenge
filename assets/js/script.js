94db19c21f12799c8cc0d90af21526bcab06adc8
TOVTC <veronica.tc.to@outlook.com>
2022-03-19 22:47:10 -0400

    added clickHandler() to progress through the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,6 @@
 // secondTestElContainer.appendChild(secondTestEl);
 // mainEl.appendChild(secondTestElContainer);
 
-//on page startup, automatically run a function that generates an h1 title, p element, and a button
-//when that button is clicked, delete the existing elements, and run a for loop
-//when the start button is clicked, a question "form" is dynamically created
-//the for loop will pull information from an array that contains multiple arrays, each of its own property
-//questions are an h1 heading and multiple choice answers are ordered list items
-//the for loop will automatically assign each element with the property values
-//the for loop will also update a "correctChoice" variable to have a value of the answer's array index value
-//questions have multiple choice answers, so an event listener for "click" needs to be added to the question form
-//to check if the answer is correct, the array index value must equal to the correctChoice index value
-//on click, when the event.target is correct, display the correct! subheading load the next dynamic set of quesions
 //if the event.target is incorrect, display the wrong! subheading and decrease the amount of time left
 //when time runs out, a form field needs to be generated and that data stored in localStorage
 //view high scores button in header should have an on click event listener
@@ -44,115 +34,111 @@ var listEl = document.createElement("ol");
 quizEl.appendChild(titleEl);
 quizEl.appendChild(listEl);
 
+//question counter
+var i = 0;
+var iterator = function() {
+    i = i+1;
+};
+
+//correct answer
+correctResponse = ""
+
 //set up for intro page
 var introObject = function() {
     titleEl.textContent = "Coding Quiz Challenge"
     var paraEl = document.createElement("p");
     paraEl.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
     quizEl.appendChild(paraEl);
-    var introButtonEl = document.createElement("button");
-    introButtonEl.textContent = "Start Quiz";
-    quizEl.appendChild(introButtonEl);
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "Start Quiz";
+    startButtonEl.className = "startButtonEl";
+    quizEl.appendChild(startButtonEl);
 }
 
-// //turn questions into objects
-// var qOne = {
-//     question: "Commonly used data types do NOT include:",
-//     choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-//     correct: "" //Alerts
-// }
-// var qTwo = {
-//     question: "The condition in an if/else statement is enclosed in:",
-//     choices: ["Quotes", "Parentheses", "Curly Brackets", "Square Brackets"],
-//     correct: "" //Parentheses
-// }
-// var qThree = {
-//     question: "Arrays in JavaScript can be used to store",
-//     choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
-//     correct: "" //All of the Above
-// }
-// var qFour = {
-//     question: "String values must be enclosed within __________ when being assigned to variables",
-//     choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
-//     correct: "" //Quotes
-// }
-// var qFive = {
-//     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-//     choices: ["JavaScript", "Terminal Bash", "for loops", "console.log"],
-//     correct: "" //console.log
-// }
-
-
+//add a clickHandler function that redirects all functions based on what was clicked
+var clickHandler = function(event) {
+    var targetEl = event.target;
+    if (targetEl.matches(".startButtonEl")) {
+        startQuiz();
+    } else if (targetEl.textContent === correctResponse) {
+        correctAnswer();
+    } else if (targetEl.textContent !== correctResponse) {
+        incorrectAnswer();
+    }
+};
 
 //add a startQuiz function that executes when the quiz button is clicked that clears the introObject items, starts the timer, and runs the formUpdate function
+var startQuiz = function() {
+    var startButtonEl = document.querySelector(".startButtonEl");
+    startButtonEl.remove();
+    quizUpdate();
+};
+
+//correct answer was selected
+var correctAnswer = function() {
+    console.log("correct!")
+    quizUpdate();
+}
+
+//incorrect answer was selected
+var incorrectAnswer = function() {
+    console.log("incorrect!")
+    quizUpdate();
+}
 
 //add questions to an array
 var questions = [{
     question: "Commonly used data types do NOT include:",
     choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-    correct: "" //Alerts
+    correct: "Alerts"
 }, {
     question: "The condition in an if/else statement is enclosed in:",
     choices: ["Quotes", "Parentheses", "Curly Brackets", "Square Brackets"],
-    correct: "" //Parentheses
+    correct: "Parentheses"
 }, {
     question: "Arrays in JavaScript can be used to store",
     choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
-    correct: "" //All of the Above
+    correct: "All of the Above"
 }, {
     question: "String values must be enclosed within __________ when being assigned to variables",
     choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
-    correct: "" //Quotes
+    correct: "Quotes"
 }, {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "Terminal Bash", "for loops", "console.log"],
-    correct: "" //console.log
-}]
-
-// //update update the information
-// var quizUpdate = function() {
-//     for (i = 0; i < questions.length; i++) {
-//         //reset listEl
-//         listEl.innerHTML = "";
-//         titleEl.textContent = questions[i].question;
-//         //a ul is composed of li items, so create an li item and use array at index [j] to assign the text value of each li item
-//         //for the length of the choices array of the question in the questions array at the index of the loop being run, create an li element and assign it to the ol element
-//         for (j = 0; j < questions[i].choices.length; j++) {
-//             var item = document.createElement("li");
-//             item.textContent = questions[i].choices[j];
-//             listEl.appendChild(item);
-//         }
-//         return;
-//     }
-// }
-
-
-var i = 0;
-var iterator = function() {
-    i = i+1
-    console.log(i);
-}
+    correct: "console.log"
+}];
 
 //update the quiz information
 var quizUpdate = function() {
-    titleEl.textContent = questions[i].question;
-    arrayToList();
-    iterator();
-}
+    if (i < questions.length) {
+        titleEl.textContent = questions[i].question;
+        arrayToList();
+        correctChoice();
+        iterator();
+    };
+};
 
-//a ul is composed of li items, so create an li item and use array at index [j] to assign the text value of each li item
-//for the length of the choices array of the question in the questions array at the index of the loop being run, create an li element and assign it to the ol element
+//an ol is composed of li items, so create an li item and use array at index [j] to assign the text value of each li item
 var arrayToList = function(){
     //reset listEl
     listEl.innerHTML = "";
+    //for the length of the choices array of the question in the questions array at the index of the loop being run, create an li element and assign it to the ol element
     for (j = 0; j < questions[i].choices.length; j++) {
         var item = document.createElement("li");
         item.textContent = questions[i].choices[j];
         listEl.appendChild(item);
-    }
-}
+    };
+};
+
+//update correct answer in each question
+var correctChoice = function() {
+    if (i < questions.length) {
+        correctResponse = questions[i].correct;
+    };
+};
 
 //add an event listener for the form to enable button functionality
-quizEl.addEventListener("click", quizUpdate);
+quizEl.addEventListener("click", clickHandler);
 
 introObject();
